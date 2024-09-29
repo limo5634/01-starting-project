1198ac6d915b13da4c18247cb5c55759b312dde9
Felix Köhler <fkoehler11@web.de>
2024-09-29 16:26:17 +0200

    first commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,34 @@
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  input,
+  Output,
+  output,
+} from '@angular/core';
+
+import { User } from './user.model';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Input({required: true}) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+    // console.log('click')
+  }
+}
